2e78c87de2aefa7cd5a90cf8262a695d6cdd0e34
nmelgar <nmelgar16013@gmail.com>
2025-02-08 22:59:15 -0600

    Project work to add a book.

--- services/add_book.py
+++ services/add_book.py
@@ -1,12 +1,32 @@
-import db.db_conn as db_conn
 import sys
 import os
 
+# Add the parent directory to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+from db.db_conn import get_db_connection
 
-# mydb = db_conn.get_db_connection()
-# mycursor = mydb.cursor()
 
 def add_book():
-    print("add book")
+    booksdb = get_db_connection()
+    dbcursor = booksdb.cursor()
+    print(
+        "\n\n|---------------------------------------------|\n|---------------- Add Book -------------------|\n|---------------------------------------------|\n"
+    )
+    print("\n|-- Add the information for the book --|")
+    print("|------(R) = Required information -----|")
+    print("|--------------------------------------|")
+
+    # information to be inserted
+    book_name = input("Book name (R): ")
+    isbn13 = input("ISBN13 (R): ")
+    isbn10 = input("ISBN10 (R): ")
+    author = input("Author (R): ")
+
+    sql = "INSERT INTO books (isbn13, isbn10, title, author) VALUES (%s, %s, %s, %s)"
+    val = (isbn13, isbn10, book_name, author)
+    dbcursor.execute(sql, val)
+
+    booksdb.commit()
+
+    print(dbcursor.rowcount, "\nBook succesfully.")
